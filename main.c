3208fbbd56c3185c34cec42cd498ed7afb52e55d
matheus anmo <matheusanmo@gmail.com>
2021-11-06 19:07:41 -0300

    sequential

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>  // printf FILE fprintf fopen fclose
+#include <stdio.h>  // printf FILE fprintf fopen fclose rewind fscanf
 #include <stdlib.h> // malloc rand srand
 #include <time.h>   // time
 
@@ -34,11 +34,33 @@ void print_help() {
     return;
 }
 
+void sequential(FILE* m1, FILE* m2) {
+    rewind(m1);
+    rewind(m2);
+
+    int lines_m1, lines_m2, cols_m1, cols_m2;
+    fscanf(m1, "%d %d\n", &lines_m1, &cols_m1);
+    fscanf(m2, "%d %d\n", &lines_m2, &cols_m2);
+    printf("%d %d\n%d %d\n", lines_m1, cols_m1, lines_m2, cols_m2);
+
+    int elem;
+    for (int i = 0; i < lines_m1; i++) {
+        for (int j = 0; j < cols_m1; j++) {
+            fscanf(m1, "%d", &elem);
+            printf("%d ", elem);
+        }
+        printf("\n");
+    }
+
+    return;
+}
+
 int main(int argc, char* argv[]) {
-    FILE* file1 = fopen("m1", "wt");
-    FILE* file2 = fopen("m2", "wt");
+    FILE* file1 = fopen("m1", "w+t");
+    FILE* file2 = fopen("m2", "w+t");
     auxiliar(3, 3, file1);
     auxiliar(3, 3, file2);
+    sequential(file1, file2);
     fclose(file1);
     fclose(file2);
     return 0;
