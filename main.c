cfbda30166c428995fb58a3ea9f7d58d108ee977
matheus anmo <matheusanmo@gmail.com>
2021-11-06 18:29:46 -0300

    auxiliar I

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>  // printf FILE fprintf
+#include <stdlib.h> // malloc rand srand
+#include <time.h>   // time
+
+void auxiliar(int l, int c, FILE* fhandle) {
+    fprintf(fhandle, "%d %d\n", l, c);
+
+    // nao fazer nada quando for pedida matrix de tamanho indefinido
+    if (l <= 0 || c <= 0) { 
+        return;
+    }
+
+    srand((unsigned) time(NULL)); // usando time como seed pro rng
+
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < c; j++) {
+            fprintf(fhandle, "%d ", rand());
+        }
+        fprintf(fhandle, "\n");
+    }
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    FILE* file1 = fopen("m1", "wt");
+    FILE* file2 = fopen("m2", "wt");
+    auxiliar(3, 3, file1);
+    auxiliar(3, 3, file2);
+    fclose(file1);
+    fclose(file2);
+    return 0;
+}
+
