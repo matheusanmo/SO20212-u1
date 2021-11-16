8fda3e8ce0b2cf0eefe8ae8d540c0a9d20e03090
matheus anmo <matheusanmo@gmail.com>
2021-11-16 17:48:50 -0300

    matrix refatorado

---
+++ matrix.c
@@ -0,0 +1,63 @@
+#include "matrix.h"
+#include <stdlib.h> // malloc free NULL
+#include <stdio.h>  // FILE fprintf fopen fclose
+
+Matrix matrix_new(int lines, int columns) {
+    if (lines <= 0 || columns <= 0)
+        return (Matrix){ lines, columns, NULL };
+    return (Matrix){ lines, columns, malloc(sizeof(int) * lines * columns) };
+}
+
+void matrix_destroy(Matrix* m) {
+    m->lines   = -1;
+    m->columns = -1;
+    free(m->elems);
+    m->elems   = NULL;
+}
+
+int matrix_write(char* filepath, Matrix m) {
+    // escrevendo linhas e colunas na primeira linha 
+    FILE* fhandle = fopen(filepath, "wt");
+    fprintf(fhandle, "%d %d\n", m.lines, m.columns);
+
+    // salvar matrix no arquivo de texto
+    int i_elem = 0;
+    for (int i = 0; i < m.lines; i++) {
+        for (int j = 0; j < m.columns; j++) {
+            fprintf(fhandle, "%d ", m.elems[i_elem]);
+            i_elem++;
+        }
+        fprintf(fhandle, "\n");
+    }
+    fclose(fhandle);
+    return 0;
+}
+
+Matrix matrix_read(char* filepath) {
+    FILE* fhandle = fopen(filepath, "rt");
+    
+    // ler dimensoes na primeira linha
+    int lines, cols;
+    fscanf(fhandle, "%d %d\n", &lines, &cols);
+
+    Matrix ret = matrix_new(lines, cols);
+
+    for (int i = 0; i < lines * cols; i++) {
+        fscanf(fhandle, "%d", &(ret.elems[i]));
+    }
+
+    fclose(fhandle);
+
+    return ret;
+}
+
+int matrix_coord_index(Matrix m, int line, int col) {
+    return (line - 1) * m.columns + (col - 1);
+}
+
+int matrix_index_coord(Matrix m, int index, int linecol[2]) {
+    linecol[0] = index / m.columns + 1;
+    linecol[1] = index % m.lines + 1;
+    return 0;
+}
+
--- matrix.h
+++ matrix.h
@@ -13,9 +13,9 @@ typedef struct Matrix Matrix;
  * Aloca (sem inicializar) matriz do tamanho pedido. Deve ser destruida com `matrix_destroy`
  * para recuperar a memoria alocada.
  * 
- * @param lines   quantidade de linhas da matrix a ser alocada.
- * @param columns quantidade de colunas da matrix a ser alocada.
- * @return `Matrix` com tamanho pedido.
+ * @param lines   quantidade de linhas da matrix a ser alocada
+ * @param columns quantidade de colunas da matrix a ser alocada
+ * @return `Matrix` com tamanho pedido
  * @see matrix_destroy()
  */
 Matrix matrix_new(int lines, int columns);
@@ -24,8 +24,8 @@ Matrix matrix_new(int lines, int columns);
  * Le uma matriz gravada com `matrix_write` numa `Matrix` usando `matrix_new`. A matriz
  * retornada deve ser destruida com `matrix_destroy`.
  * 
- * @param filepath string do caminho para o arquivo de texto a ser lido.
- * @return `Matrix` lida do arquivo passado.
+ * @param filepath string do caminho para o arquivo de texto a ser lido
+ * @return `Matrix` lida do arquivo passado
  * @see matrix_destroy() matrix_write()
  */
 Matrix matrix_read(char* filepath);
@@ -34,9 +34,9 @@ Matrix matrix_read(char* filepath);
  * Grava uma `Matrix` num arquivo de texto. O arquivo a ser usado sera truncado, apagando
  * qualquer conteudo anterior. O arquivo pode ser lido com `matrix_read`.
  * 
- * @param  filepath string do caminho para o arquivo de texto a ser gravado.
- * @param  m        `Matrix` a ser gravada.
- * @return numero maiores que 0 indicam erros. 0 indica sucesso.
+ * @param filepath string do caminho para o arquivo de texto a ser gravado
+ * @param m        `Matrix` a ser gravada
+ * @return numero maiores que 0 indicam erros. 0 indica sucesso
  * @see matrix_read()
  */
 int matrix_write(char* filepath, Matrix m);
@@ -45,9 +45,9 @@ int matrix_write(char* filepath, Matrix m);
  * Retorna o indice de um elemento duma matriz a partir das suas coordenadas 1-indexadas.
  * Isto e, quando `line=col=1` retorna-se o indice `0`.
  * 
- * @param line linha do elemento desejado, contando a partir de 1.
- * @param col  coluna do elemento desejado, contando a partir de 1.
- * @return indice do elemento desejado ou -1 em caso de erro.
+ * @param line linha do elemento desejado, contando a partir de 1
+ * @param col  coluna do elemento desejado, contando a partir de 1
+ * @return indice do elemento desejado ou -1 em caso de erro
  * @see matrix_index_coord()
  */
 int matrix_coord_index(Matrix m, int line, int col);
@@ -57,7 +57,7 @@ int matrix_coord_index(Matrix m, int line, int col);
  * a linha em `linecol[0]` e a coluna em `linecol[1]`. Ambos sao 1-indexados. 
  * Retorna numero positivo em caso de erro. 0 indica sucesso.
  * 
- * @param index indice 0-indexado da matriz e.g. `matriz.data[5]` 
+ * @param      index   indice 0-indexado da matriz e.g. `matriz.data[5]` 
  * @param[out] linecol buffer para salvar linha e coluna do elemento buscado
  * @return 0 indica sucesso, numeros positivos indicam erro
  * @see matrix_coord_index()
@@ -74,3 +74,4 @@ int matrix_index_coord(Matrix m, int index, int linecol[2]);
 void matrix_destroy(Matrix* m);
 
 #endif
+
