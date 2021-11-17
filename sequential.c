9b4ec1a9b100fcad3ea5a766151d68575322b03a
matheus anmo <matheusanmo@gmail.com>
2021-11-17 11:44:54 -0300

    sequential ok

--- sequential.c
+++ sequential.c
@@ -1,9 +1,10 @@
 #include "sequential.h"
 #include "timetools.h"
 #include "matrix.h"
-#include <stdio.h> // printf
+#include <stdio.h> // printf fopen FILE fprintf fclose
 
 void sequential(char* m1_path, char* m2_path, char* mout_path) {
+    printf("sequential com m1='%s', m2='%s', m3='%s'\n", m1_path, m2_path, mout_path);
     Matrix m1   = matrix_read(m1_path);
     Matrix m2   = matrix_read(m2_path);
 
@@ -14,13 +15,32 @@ void sequential(char* m1_path, char* m2_path, char* mout_path) {
         return;
     }
 
-    int elem;
-    for (int i = 0; i < m1.lines; i++) {
-        for (int j = 0; j < m1.columns; j++) {
-            printf("%d ", elem);
+    Matrix mout = matrix_new(m1.lines, m2.columns);
+
+    struct timeval t0 = timeval_now();
+    for (int i = 1; i <= mout.lines; i++) {
+        for (int j = 1; j <= mout.columns; j++) {
+            int cij = 0; // cij = sigma_{k=1}^n a[i,k]*b[k,j]
+            for (int k = 1; k <= m1.columns; k++) { // n = dimensao comum entre m1 m2
+                int aik = m1.elems[matrix_coord_index(m1, i, k)];
+                int bkj = m2.elems[matrix_coord_index(m2, k, j)];
+                cij += aik * bkj;
+            }
+            mout.elems[matrix_coord_index(mout, i,j)] = cij;
         }
-        printf("\n");
     }
+    struct timeval t1 = timeval_now();
+
+    // salvar matriz, calcular tempo de execucao e append na matriz saida
+    matrix_write(mout_path, mout);
+    long dt = elapsed_miliseconds(t0, t1);
+    FILE* outhandle = fopen(mout_path, "at");
+    fprintf(outhandle, "%ldms\n", dt);
+    fclose(outhandle);
+
+    matrix_destroy(&m1);
+    matrix_destroy(&m2);
+    matrix_destroy(&mout);
     return;
 }
 
